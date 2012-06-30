1908e37e389e15059933dc16d9e91f12f8bad614
Mihail Buryakov <mburyakov@mail.ru>
2012-06-30 20:34:42 +0400

    Extended type Value

--- Types.hs
+++ Types.hs
@@ -1,6 +1,8 @@
 module Types where
 
 import qualified Data.Map as M
+import Data.Bits
+import Data.Int
 
 data Insn =
     Label String Insn
@@ -16,12 +18,93 @@ data Insn =
   | Spawn String Prog
   | Assert String
 
-data Value = IntValue Int | BoolValue Bool | PidValue Pid deriving (Ord, Eq)
+--All integer value are unsigned (machine-representation).
+--Signed values are simulated with labels
+data Value =
+    BoolValue Bool 
+  | IntValue Int8           --TODO:unsigned
+  | SmallIntValue {valSize ::Int,   siValue::Int32,   valLabel::String} --TODO:unsigned
+  | BigIntValue   {valSize ::Int,   biValue::Integer, valLabel::String} --TODO:unsigned
+  | EnumValue     {valSpace::Int32, enValue::Int32 } --TODO:unsigned
+  | BitSetValue   {                 bsValue::[Bool] }
+  | PidValue Pid deriving (Ord, Eq)
 
 instance Show Value where
-  show (IntValue i) = show i
   show (BoolValue b) = show b
+  show (IntValue i) = show i
+  show (SmallIntValue _ i label) = show i ++ "/" ++ label ++ "/"
+  show (BigIntValue _ i label) = show i ++ "/" ++ label ++ "/"	
+  show (BitSetValue l) = "[" ++ map (\b -> if b then '1' else '0') l ++ "]"
   show (PidValue p) = show p
+
+{-fullCheckValue v = 
+  checkValue v && checkValType t && checkValBinary v b
+    where
+      t = valueType v
+      b = vatToBinary v
+-}
+-- is value acceptable?
+checkValue :: Value -> Bool
+checkValue (BoolValue _) = True
+checkValue (IntValue _)  = True
+checkValue (SmallIntValue s v _) = (intBinSize v) <= s
+checkValue (BigIntValue   s v _) = (intBinSize v) <= s
+checkValue (EnumValue     sp v) = v < sp
+checkValue (BitSetValue l) = (length l)<=32
+checkValue (PidValue _) = False
+
+data ValueType =
+    BoolType
+  | IntType
+  | SmallIntType Int --TODO:unsigned
+  | BigIntType Int --TODO:unsigned
+  | EnumType Int32 --TODO:unsigned
+  | BitSetType Int --TODO:unsigned
+  | PidType
+
+valType :: Value -> ValueType
+valType (BoolValue        _) = BoolType
+valType (IntValue         _) = IntType
+valType (SmallIntValue s  _ lab) = SmallIntType s
+valType (BigIntValue   s  _ lab) = BigIntType s
+valType (EnumValue     sp _) = EnumType sp
+valType (BitSetValue      l) = BitSetType $ length l
+valType (PidValue         _) = PidType
+
+-- is type acceptable?
+checkValType :: ValueType -> Bool
+checkValType BoolType = True
+checkValType IntType = True
+checkValType (SmallIntType s) = s <= 32
+checkValType (BigIntType s) = True
+checkValType (EnumType sp) = sp>0
+checkValType (BitSetType s) = s<=32
+checkValType PidType = False
+
+bitsToBinary :: Bits a => a -> [Bool]
+bitsToBinary i =
+  map (testBit i) [0..bitSize i-1]
+
+intBinSize :: Bits a => a -> Int
+intBinSize v = 
+  length $ dropWhile not $ reverse $ bitsToBinary v
+
+binSize :: ValueType -> Int
+binSize BoolType = 1
+binSize IntType = 8
+binSize (SmallIntType s) = s
+binSize (BigIntType s) = s
+binSize (EnumType sp) = intBinSize (sp-1)
+binSize (BitSetType s) = s
+binSize PidType = error "binSize PidType"
+
+valToBinary :: Value -> [Bool]
+valToBinary (BoolValue b) = [b]
+valToBinary (IntValue i) = bitsToBinary i
+valToBinary (SmallIntValue s i label) = take s (bitsToBinary i)
+--valToBinary (BigIntValue s i label) = 
+--valToBinary (BitSetValue l) = 
+--valToBinary (PidValue p) = 
 
 data Pid = Pid Int deriving (Eq, Ord)
 
