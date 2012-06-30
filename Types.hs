6ba301891fc13989e2df925b175d8194ce933075
Mihail Buryakov <mburyakov@mail.ru>
2012-06-30 17:40:08 +0400

    heterogeneous types: Value, Stack

--- Types.hs
+++ Types.hs
@@ -1,61 +1,91 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE DeriveDataTypeable #-}
+--{-# LANGUAGE ExistentialQuantification #-}
+--{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+--{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
+--{-# LANGUAGE OverlappingInstances #-}
+{-# LANGUAGE DatatypeContexts #-}
+--{-# LANGUAGE GADTs #-}
 
 module Types where
 
 import qualified Data.Map as M
 import Data.Bits
-import Data.Typeable
-import Data.Boolean
+--import Data.Typeable
+--import Data.Boolean
+import SimpleHList
 
-data Insn =
-    Label String Insn
-  | Block [Insn]
-  | Jmp String
+data Insn i o =
+--    Label String Insn
+--  | Block [Insn]
+  Jmp String
   | JmpCond String
   | Get String
   | Set String
-  | Arith ([Value] -> [[Value]])
+  | Arith (Arithmetic i o)
   | Enter String
   | TryEnter String
   | Leave String
-  | Spawn String Prog
+--  | Spawn String Prog
   | Assert String
-  
-class (Show a, Typeable a) => Value_ a where
-  valLength   :: a -> Int    -- independent of argument
-  valToBinary :: a -> [Bool] -- TODO: wanted: length $ valToBinary x == valLength x (for all x) 
-  valLength x = length $ valToBinary x
-  
-data Value = forall a. Value_ a => Value a
 
-instance Show (Value) where
-  show (Value x) = show x
-instance Eq (Value) where
-  (Value x) == (Value y) = (typeOf x == typeOf y) && (valToBinary x == valToBinary y)
-instance Ord (Value) where
-  compare (Value x) (Value y) = case typeCmp of
-      EQ -> valCmp
-      _  -> typeCmp
+data (Stack i, Stack o) => Arithmetic i o = Arithmetic {
+  arOp     :: (i -> [o]),
+  --arInput  :: [Int],
+  --arOutput :: [Int],
+  predicate:: [[Bool]] -> [[Bool]] -> Bool
+}
+
+arNeg = Arithmetic {
+  arOp = \(HCons (BoolValue a) HNil) -> [HCons (BoolValue (not a)) HNil],
+  predicate = undefined
+}
+
+applyArith arith input = 
+  hConcat outp rest
     where
-      typeCmp = compare (typeOf x)   (typeOf y)
-      valCmp = compare (valToBinary x) (valToBinary y)
-      
-data EnumValue a = EnumValue a deriving (Show, Typeable)
-instance (Show a, Bits a, Typeable a) => Value_ (EnumValue a) where
-  valLength   (EnumValue x) = bitSize x
-  valToBinary v@(EnumValue x) = map (testBit x) [0..(valLength v)]
+      (inp, rest) = (hTakeDrop input)
+      outp = head (op inp)
+      op = arOp arith
 
-data BoolValue = BoolValue Bool deriving (Show, Typeable)
-instance Value_ BoolValue where
-  valLength _ = 1
-  valToBinary (BoolValue x) = [x]
+class Stack s where
+  stackToBinList :: s -> [[Bool]]
+
+instance Stack (HNil) where
+  stackToBinList _ = []
+
+instance (Value v, Stack s, HList s) => Stack (HCons v s) where
+  stackToBinList (HCons value tail) = 
+    toBinary value : stackToBinList tail
+
+class Binarizable a where
+  binLength   :: a -> Int    -- independent of argument
+  toBinary :: a -> [Bool] -- TODO: wanted: length $ toBinary x == binLength x (for all x) 
+  binLength x = length $ toBinary x
+
+class Binarizable a => Value a
+
+checkValLength x = binLength x == length (toBinary x) -- it must be always True
+      
+data EnumValue a = EnumValue a deriving Show
+instance Bits a => Value (EnumValue a)
+instance Bits a => Binarizable (EnumValue a) where
+  binLength   (EnumValue x) = bitSize x
+  toBinary v@(EnumValue x) = map (testBit x) [0..(binLength v)-1]
+
+data BoolValue = BoolValue Bool deriving Show
+instance Value BoolValue
+instance Binarizable BoolValue where
+  binLength _ = 1
+  toBinary (BoolValue x) = [x]
 
 --TODO: do not push PidValue when spawning
-data PidValue  = PidValue Pid deriving (Show, Typeable)
-instance Value_ PidValue where
-  valLength _ = error "I'm a PidValue - your memory leak!"
-  valToBinary _ = error "I'm a PidValue - do not disturb me!"
+data PidValue  = PidValue Pid deriving Show
+instance Value PidValue
+instance Binarizable PidValue where
+  binLength _ = error "I'm a PidValue - your memory leak!"
+  toBinary _ = error "I'm a PidValue - do not disturb me!"
   
 --There is no IntValue because of unknown length (Int8, Int16, ...)
 
@@ -64,11 +94,40 @@ data Pid = Pid Int deriving (Eq, Ord)
 instance Show Pid where
   show (Pid p) = show p
 
+class Env e where
+  envToBinList :: e -> [(String, [Bool])]  
+
+instance Env (HNil) where
+  envToBinList _ = []
+
+instance (Value v, Env e, HList e) => Env (HCons (String, v) e) where
+  envToBinList (HCons (name, value) tail) = 
+    (name, toBinary value) : envToBinList tail
+
+data (Stack s, Env e) => Point s e = Point {
+  pointStack :: s,
+  pointEnv   :: e
+}
+
+first  (x,y) = x
+second (x,y) = y
+
+instance (Stack s, Env e) => Binarizable (Point s e) where
+  toBinary point = 
+    envbin ++ stackbin
+      where
+        envbin   = concat $ map second $ envToBinList env
+	stackbin = concat $ stackToBinList stack
+	env = pointEnv point
+	stack = pointStack point
+  
+
+{--
 data Prog = Prog 
   { 
     prog_insns :: [Insn] 
   }
-  
+
 data Model = Model
   {    
     mod_vars :: [(String,Value)],
@@ -123,3 +182,4 @@ compile is = Prog {prog_insns = expandBlocks is}
   where
     expandBlocks = concatMap (\i -> case i of { Block is -> expandBlocks is ; j -> [j] })
 
+--}
